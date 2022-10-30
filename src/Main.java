eb988c6f7121360ff3f2191371cdaee390a891dd
Jihun Choi <98497929+Choijh03@users.noreply.github.com>
2022-10-30 16:33:03 -0700

    Main code has been reformatted
    Main code has been reformatted

--- src/Main.java
+++ src/Main.java
@@ -14,43 +14,29 @@ public class Main {
             //int[][] test1 = {{1,0, 1, 1 }, {1,0, 1, 1}, {1,0, 1, 1}, {1,0, 1, 1}};
             //int[][] test2 = {{1,0, 1, 1}, {1,0, 1, 1}, {1,0, 1, 1}, {1,0, 1, 1}};
 
-            print2D(test1);
-            print2D(test2);
+           System.out.println("Test1 Matrix: ");
+           tester.printMat(test1);
+           System.out.println("Test2 Matrix: ");
+           tester.printMat(test2);
 
-            //Checking for classical method
-            int[][] result1 = tester.classical(test1,test2,test1.length);
-            System.out.println("Result of Classical method: ");
-            print2D(result1);
+           //Checking for classical method
+           int[][] result1 = tester.classical(test1,test2,test1.length);
+           System.out.println("Result of Classical method: ");
+           tester.printMat(result1);
 
-            //Checking for Divide & Conquer method
-            int[][] result2 = tester.divideAndConquer(test1,test2, test1.length);
-            System.out.println("Result of Divide and Conquer method: ");
-            print2D(result2);
+           //Checking for Divide & Conquer method
+           int[][] result2 = tester.divideAndConquer(test1,test2, test1.length);
+           System.out.println("Result of Divide and Conquer method: ");
+           tester.printMat(result2);
 
-            //Checking for classical method
-            int[][] result3 = tester.strassen(test1,test2, test1.length);
-            System.out.println("Result of Strassen method: ");
-            print2D(result3);
-            //calling run time test method
-            //data(test, 0);
-        } // end main
+           //Checking for classical method
+           int[][] result3 = tester.strassen(test1,test2, test1.length);
+           System.out.println("Result of Strassen method: ");
+           tester.printMat(result3);
 
-    public static void print2D(int[][] arr) {
-        for (int[] i : arr) {
-            for (int j : i) {
-                System.out.printf("%3d ", j);
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-    /*
-    public static void main(String[] args) {
-        int[][] test0 = {{0}};
-        int[][] test = {{2,0, -1, 6}, {3,7, 8, 0}, {-5, 1, 6, -2}, {8, 0, 2, 7}};
-        //int[][] testN = new int[n][n];
-        data(test, 0);
-    } // end main
+           data(test1, 0);
+
+       } // end main
 
     public static void data(int[][] m, int fill) {
         int l = 2;
@@ -64,8 +50,8 @@ public class Main {
     public static void timeCounter(int[][] A) {
         long start, run, avg = 0;
         start = System.currentTimeMillis();
-        //testClassical(A);
-        testDivConquer(A);
+        testClassical(A);
+        //testDivConquer(A);
         //testStrassen(A);
         run = System.currentTimeMillis() - start;
 
@@ -77,5 +63,6 @@ public class Main {
     public static void testDivConquer(int[][] a) {tester.divideAndConquer(a, a, a.length);}    // end testDivConquer
     public static void testStrassen(int[][] a) {tester.strassen(a, a, a.length);}        // end testStrassen
 
-    */
+
+
 } // end Main
