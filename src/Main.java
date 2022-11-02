e24c7f34c0510b33b266cadf38391e6c7dd0e1d4
Jihun Choi <98497929+Choijh03@users.noreply.github.com>
2022-11-02 15:01:31 -0700

    Main method update
    Main method has been re-organized to be able to distinguish between 3 different tests (regular Sanity check, Extra credit Sanity Check, Time Comparison)

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,118 @@
 /*
  * Project#1 Matrix Multiplication
- * Test and Analysis of Classical-O(n^3)(Brute Force/Classical Method), Divide and Conquer Methods-O(n^3) and Strassen Algorithm O(n^2.8)
+ * Test and Analysis of Classical-O(n^3)(Brute Force Method), Divide and Conquer Methods-O(n^3) and Strassen Algorithm O(n^2.8)
  * Myo Aung & Jihun Choi
  */
-
 public class Main {
     static Matrix tester = new Matrix();
-    public static void main(String[] args) {
 
-        //Sanity check
-        //int[][] test1 = {{3, 5, 1, 3}, {1, 2, 3, 4}, {4, 5, 6, 8}, {7, 8, 9, 3}};
-        //int[][] test2 = {{4, 1, 2, 3}, {1, 2, 1, 6}, {2, 4, 6, 2}, {6, 2, 5, 4}};
-        int[][] test1 = {{2, 1, 1, 5, 6}, {3, 7, 3, 8, 3}, {4, 7, -3, 6, -9}, {2, 7, -3, 6, -9}, {4, -7, -3, 6, -9}};
-        int[][] test2 = {{2, 1, 1, 0, 6}, {3, -7, 3, -8, 3}, {1, 5, -3, 6, -9}, {-2, 7, -3, 6, -9}, {4, -7, -3, 6, -9}};
-        //int[][] test1 = {{1,0, 1, 1 }, {1,0, 1, 1}, {1,0, 1, 1}, {1,0, 1, 1}};
-        //int[][] test2 = {{1,0, 1, 1}, {1,0, 1, 1}, {1,0, 1, 1}, {1,0, 1, 1}};
+   public static void main(String[] args) {
 
-        printMatrix(test1);
-        printMatrix(test2);
+           //Sanity check
+           int[][] test1 = {{2,0, -1, 6}, {3,7, 8, 0}, {-5, 1, 6, -2}, {8, 0, 1, 7}};
+           int[][] test2 = {{0,1, 6, 3}, {-2,8, 7, 1}, {2, 0, -1, 0}, {9, 1, 6, -2}};
+           int[][] test3 = {{2, 1, 1, 5, 6}, {3, 7, 3, 8, 3}, {4, 7, -3, 6, -9}, {2, 7, -3, 6, -9}, {4, -7, -3, 6, -9}};
+           int[][] test4 = {{2, 1, 1, 0, 6}, {3, -7, 3, -8, 3}, {1, 5, -3, 6, -9}, {-2, 7, -3, 6, -9}, {4, -7, -3, 6, -9}};
+           //int[][] test1 = {{1,0, 1, 1 }, {1,0, 1, 1}, {1,0, 1, 1}, {1,0, 1, 1}};
+           //int[][] test2 = {{1,0, 1, 1}, {1,0, 1, 1}, {1,0, 1, 1}, {1,0, 1, 1}};
 
-        if (chekPowerOfTwo(test1)) {
-            //Checking for classical method
-            int[][] result1 = tester.classical(test1, test2, test1.length);
-            System.out.println("Result of Classical method: ");
-            printMatrix(result1);
+       //Sanity Check for regular setup
+       /*
+           System.out.println("Test1 Matrix: ");
+           tester.printMat(test1);
+           System.out.println("Test2 Matrix: ");
+           tester.printMat(test2);
 
-            //Checking for Divide & Conquer method
-            int[][] result2 = tester.divideAndConquer(test1, test2, test1.length);
-            System.out.println("Result of Divide and Conquer method: ");
-            printMatrix(result2);
+           //Checking for classical method
+           int[][] result1 = tester.classical(test1,test2,test1.length);
+           System.out.println("Result of Classical method: ");
+           tester.printMat(result1);
 
-            //Checking for strassen method
-            int[][] result3 = tester.strassen(test1, test2, test1.length);
-            System.out.println("Result of Strassen method: ");
-            printMatrix(result3);
+           //Checking for Divide & Conquer method
+           int[][] result2 = tester.divideAndConquer(test1,test2, test1.length);
+           System.out.println("Result of Divide and Conquer method: ");
+           tester.printMat(result2);
 
-        } else {
-            System.out.println("The matrix is NOT power of 2!!");
-            System.out.println("The Matrix Length is: " + test1.length);
-            System.out.println();
-            int[][] newtest1 = padArray(test1, 1, 3);
-            int[][] newtest2 = padArray(test2, 1, 3);
-            int[][] result1 = tester.classical(newtest1, newtest2, newtest2.length);
-            //print2D(newtest1);
-            System.out.println("Result of Classical method: ");
-            printMatrix(result1);
+           //Checking for classical method
+           int[][] result3 = tester.strassen(test1,test2, test1.length);
+           System.out.println("Result of Strassen method: ");
+           tester.printMat(result3);
+        */
 
-            //Checking for Divide & Conquer method
-            int[][] result2 = tester.divideAndConquer(newtest1, newtest2, newtest2.length);
-            System.out.println("Result of Divide and Conquer method: ");
-            printMatrix(result2);
 
-            //Checking for strassen method
-            int[][] result3 = tester.strassen(newtest1, newtest2, newtest2.length);
-            System.out.println("Result of Strassen method: ");
-            printMatrix(result3);
-        }
+       //Sanity Check for Extra credit
+       System.out.println("Test3 Matrix: ");
+       tester.printMat(test3);
+       System.out.println("Test4 Matrix: ");
+       tester.printMat(test4);
 
-    }
+       if (chekPowerOfTwo(test3)) {
+           //Checking for classical method
+           int[][] result1 = tester.classical(test3, test4, test3.length);
+           System.out.println("Result of Classical method: ");
+           tester.printMat(result1);;
+
+           //Checking for Divide & Conquer method
+           int[][] result2 = tester.divideAndConquer(test3, test4, test3.length);
+           System.out.println("Result of Divide and Conquer method: ");
+           tester.printMat(result2);
+
+           //Checking for strassen method
+           int[][] result3 = tester.strassen(test3, test4, test3.length);
+           System.out.println("Result of Strassen method: ");
+           tester.printMat(result3);
+
+       } else {
+           System.out.println("The matrix is NOT power of 2!!");
+           System.out.println("The Matrix Length is: " + test1.length);
+           System.out.println();
+           int[][] newtest1 = padArray(test3, 1, 3);
+           int[][] newtest2 = padArray(test4, 1, 3);
+           int[][] result1 = tester.classical(newtest1, newtest2, newtest2.length);
+           System.out.println("Result of Classical method: ");
+           tester.printMat(result1);
+
+           //Checking for Divide & Conquer method
+           int[][] result2 = tester.divideAndConquer(newtest1, newtest2, newtest2.length);
+           System.out.println("Result of Divide and Conquer method: ");
+           tester.printMat(result2);
+
+           //Checking for strassen method
+           int[][] result3 = tester.strassen(newtest1, newtest2, newtest2.length);
+           System.out.println("Result of Strassen method: ");
+           tester.printMat(result3);
+       }
+
+   } // end main
+
+
+    public static void data(int[][] m, int fill) {
+        int l = 2;
+        int[][] size = tester.resize(m, l, fill);
+        for (int i = 0; i < 16; i++) {
+            timeCounter(size);
+            l *= 2;
+            size = tester.resize(m, l, fill);
+        } // end for
+    } // end data
+    public static void timeCounter(int[][] A) {
+        long start, run, avg = 0;
+        start = System.currentTimeMillis();
+        testClassical(A);
+        //testDivConquer(A);
+        //testStrassen(A);
+        run = System.currentTimeMillis() - start;
+
+        int l = A.length;
+        System.out.println(l + "*" + l + " runtime:" + run);
+    } // end timeCounter
+
+
+    public static void testClassical(int[][] a) {tester.classical(a, a, a.length);}      // end testClassical
+    public static void testDivConquer(int[][] a) {tester.divideAndConquer(a, a, a.length);}    // end testDivConquer
+    public static void testStrassen(int[][] a) {tester.strassen(a, a, a.length);}        // end testStrassen
+
+
     // Check The matrix is the size of power of TWO
     public static boolean chekPowerOfTwo(int[][] A) {
         int i = 2;
@@ -84,48 +139,4 @@ public class Main {
         return temp;
     }
 
-    public static void printMatrix(int[][] arr) {
-        for (int[] i : arr) {
-            for (int j : i) {
-                System.out.printf("%3d ", j);
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-    /*
-
-    public static void main(String[] args) {
-        int[][] test0 = {{0}};
-        //int[][] test = {{2,0, -1, 6}, {3,7, 8, 0}, {-5, 1, 6, -2}, {8, 0, 2, 7}};
-        int[][] test = {{2, 1, 1, 5, 6}, {3, 7, 3, 8, 3}, {4, 7, -3, 6, -9}, {2, 7, -3, 6, -9}, {4, -7, -3, 6, -9}};
-        //int[][] testN = new int[n][n];
-        data(test, 0);
-    }
-
-    public static void data(int[][] m, int fill) {
-        int l = 2;
-        int[][] size = tester.resize(m, l, fill);
-        for (int i = 0; i < 16; i++) {
-            timeCounter(size);
-            l *= 2;
-            size = tester.resize(m, l, fill);
-        }
-    }
-
-    public static void timeCounter(int[][] A) {
-        long start, run, avg = 0;
-        start = System.currentTimeMillis();
-        //testClassical(A);
-        //testDivConquer(A);
-        testStrassen(A);
-        run = System.currentTimeMillis() - start;
-        int l = A.length;
-        System.out.println(l + "*" + l + " runtime:" + run);
-    }
-    public static void testClassical(int[][] a) {tester.classical(a, a, a.length);}             // testClassical
-    public static void testDivConquer(int[][] a) {tester.divideAndConquer(a, a, a.length);}    // testDivConquer
-    public static void testStrassen(int[][] a) {tester.strassen(a, a, a.length);}              // testStrassen
-
-     */
-}
+} // end Main
